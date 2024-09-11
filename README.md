24a93d87b9a68aed760845ea16cda43281872b7f
Alexandria P. <alxpettit@gmail.com>
2024-09-11 15:54:57 -0600

    cleanup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Credits
+
+- Thanks to emmanuelrosa for the `mkwindowsapp` system, and for writing the [sumatrapdf-nix](https://github.com/emmanuelrosa/sumatrapdf-nix) example, on which this repo is based.
+- Thanks to cryinkfly for their [imperative repo](https://github.com/cryinkfly/Moment-of-Inspiration-MoI3D-for-Linux), which served as a MVP for this installation.
--- flake.nix
+++ flake.nix
@@ -1,25 +1,28 @@
 {
-  description = "A Nix flake for SumatraPDF";
+  description = "Moi3D for NixOS";
 
   inputs.erosanix.url = "github:emmanuelrosa/erosanix";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
 
-  outputs = { self, nixpkgs, erosanix }: {
-
+  outputs = {
+    self,
+    nixpkgs,
+    erosanix,
+  }: {
     packages.x86_64-linux = let
       pkgs = import "${nixpkgs}" {
         system = "x86_64-linux";
       };
+    in
+      with (pkgs // erosanix.packages.x86_64-linux // erosanix.lib.x86_64-linux); {
+        default = self.packages.x86_64-linux.moi3d;
 
-    in with (pkgs // erosanix.packages.x86_64-linux // erosanix.lib.x86_64-linux); {
-      default = self.packages.x86_64-linux.moi3d;
+        moi3d = callPackage ./moi3d.nix {
+          inherit mkWindowsApp makeDesktopIcon copyDesktopIcons;
 
-      moi3d = callPackage ./moi3d.nix {
-        inherit mkWindowsApp makeDesktopIcon copyDesktopIcons;
-
-        wine = wineWowPackages.full;
+          wine = wineWowPackages.full;
+        };
       };
-    };
 
     apps.x86_64-linux.moi3d = {
       type = "app";
