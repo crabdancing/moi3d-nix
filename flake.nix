e55e1cdcee3e6eb998b4f5a7cf25052fd8056af4
Alexandria P. <alxpettit@gmail.com>
2024-09-29 00:28:12 -0600

    whoops

--- flake.nix
+++ flake.nix
@@ -15,25 +15,25 @@
       pkgs = import "${nixpkgs}" {
         system = "x86_64-linux";
       };
-<<<<<<< HEAD
-      mono = pkgs.fetchurl rec {
-        version = "8.1.0";
-        url = "https://dl.winehq.org/wine/wine-mono/${version}/wine-mono-${version}-x86.msi";
-        hash = "sha256-DtPsUzrvebLzEhVZMc97EIAAmsDFtMK8/rZ4rJSOCBA=";
-      };
-    in {
-      default = self.packages.x86_64-linux.moi3d;
+      # <<<<<<< HEAD
+      #       mono = pkgs.fetchurl rec {
+      #         version = "8.1.0";
+      #         url = "https://dl.winehq.org/wine/wine-mono/${version}/wine-mono-${version}-x86.msi";
+      #         hash = "sha256-DtPsUzrvebLzEhVZMc97EIAAmsDFtMK8/rZ4rJSOCBA=";
+      #       };
+      #     in {
+      #       default = self.packages.x86_64-linux.moi3d;
 
-      moi3d = pkgs.callPackage ./moi3d.nix {
-        inherit (erosanix.lib.x86_64-linux) mkWindowsApp makeDesktopIcon copyDesktopIcons;
+      #       moi3d = pkgs.callPackage ./moi3d.nix {
+      #         inherit (erosanix.lib.x86_64-linux) mkWindowsApp makeDesktopIcon copyDesktopIcons;
 
-        wine = self.inputs.nix-gaming.packages.x86_64-linux.wine-ge.override {
-          monos = [
-            mono
-          ];
-        };
-      };
-=======
+      #         wine = self.inputs.nix-gaming.packages.x86_64-linux.wine-ge.override {
+      #           monos = [
+      #             mono
+      #           ];
+      #         };
+      #       };
+      # =======
       baseMoi3d = pkgs.callPackage ./moi3d.nix {
         inherit (erosanix.lib.x86_64-linux) mkWindowsApp makeDesktopIcon copyDesktopIcons;
         wine = pkgs.wineWowPackages.full;
@@ -46,7 +46,6 @@
       };
 
       default = self.packages.x86_64-linux.moi3d;
->>>>>>> 5b04b38 (bigdpi)
     };
 
     apps.x86_64-linux.moi3d = {
